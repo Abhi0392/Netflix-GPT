8d72e264a87ff104acd7f00140a0a552653a749a
Abhi0392 <abhishek.s.naik03@gmail.com>
2025-11-30 23:18:29 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold text-green-300">
+        Hello Lets build netflix with GPT
+      </div>
+    </>
+  );
+}
+
+export default App;
